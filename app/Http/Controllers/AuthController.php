4ad85e6dd433ab8a1a03307943c008527369dd42
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-05-22 10:57:11 +0700

    user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,6 +62,11 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
+            'user' => [
+                'name' => $user->name,
+                'phone' => $user->phone,
+                'address' => $user->address,
+            ],
         ]);
     }
 
